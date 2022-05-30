7555b15cd1d4c85999854d64ceceb462125da437
Clare Budds <clarebudds@yahoo.co.uk>
2022-05-30 20:24:14 +0100

    deleted rspec files

--- app.rb
+++ app.rb
@@ -2,9 +2,7 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 get '/cat' do
-  "<div style='border: 1px dashed red'>
-  <img src='https://i.imgur.com/jFaSxym.png'>
-  </div>"
+  erb(:index)
 end
 
 

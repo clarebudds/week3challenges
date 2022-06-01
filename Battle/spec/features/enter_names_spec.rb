74e008d38a5114c77639a78042855234f8f3716f
Clare Budds <clarebudds@yahoo.co.uk>
2022-06-01 17:50:08 +0100

    Refactored code

--- Battle/spec/features/enter_names_spec.rb
+++ Battle/spec/features/enter_names_spec.rb
@@ -1,12 +1,7 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Clare'
-    fill_in :player_2_name, with: 'Rebecca'
-    click_button 'Submit'
-
+    sign_in_and_play
     save_and_open_page 
-    
     expect(page).to have_content 'Clare vs. Rebecca'
   end
 end

107068d5d4d3da38d120aea669bb70a606c4c759
Clare Budds <clarebudds@yahoo.co.uk>
2022-06-04 14:01:03 +0100

    Added ability to Switch turns

--- Battle/spec/game_spec.rb
+++ Battle/spec/game_spec.rb
@@ -12,15 +12,28 @@ describe Game do
   end
 
   describe '#player_2' do
-  it 'retrieves the second player' do
-    expect(game.player_2.to eq player_2
+    it 'retrieves the second player' do
+      expect(game.player_2.to eq player_2
+    end
   end
-end
 
   describe '#attack' do
     it 'damages the player' do
       expect(player_2).to receive(:receive_damage)
       game.attack(player_2)
+    end
+  end
+
+  describe '#current_turn' do
+    it 'starts as player 1' do
+      expect(game.current_turn).to eq player_1
+    end
+  end
+
+  describe '#switch_turns' do
+    it 'switches the turn' do
+     game.switch_turns
+      expect(game.current_turn).to eq player_2
     end
   end
 end

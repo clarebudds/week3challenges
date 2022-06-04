481d2ea6ec9089d4c91122e20dd440430f5d4589
Clare Budds <clarebudds@yahoo.co.uk>
2022-06-04 12:29:57 +0100

    Extracted logic into Model layer

--- Battle/spec/features/attack_player2_spec.rb
+++
@@ -1,7 +0,0 @@
-feature 'Attacking' do
-  scenario 'Attack Player 2 and get confirmation' do
-    sign_in_and_play 
-    click_link 'Attack'
-    expect(page).to have_content 'Clare attacked Rebecca'
-  end
-end
--- Battle/spec/player_spec.rb
+++ Battle/spec/player_spec.rb
@@ -2,10 +2,30 @@ require 'player'
 
 RSpec.describe Player do
   subject(:clare) { Player.new('Clare') }
+  subject(:rebecca) { Player.new('Rebecca') }
 
   describe '#name' do
     it 'returns the name' do
       expect(clare.name).to eq 'Clare'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(clare.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+  
+  describe '#attack' do
+    it 'damages the player' do
+      expect(rebecca).to receive(:receive_damage)
+      clare.attack(rebecca)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the players hit points' do
+      expect { clare.receive_damage }.to change { clare.hit _points }.by(-10)
     end
   end
 end

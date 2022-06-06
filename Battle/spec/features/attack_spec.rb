7c858b268f1d03295116fdb60382395e1912c38c
Clare Budds <clarebudds@yahoo.co.uk>
2022-06-06 18:12:32 +0100

    Completed multiplayer challenge

--- Battle/spec/features/attack_spec.rb
+++ Battle/spec/features/attack_spec.rb
@@ -1,25 +1,43 @@
 require 'attack'
 
-describe Attack do
-  subject( :object) { described_class }
-  let (:player) { double :player }
-
-  describe '.run' do
-    it 'damages the player' do
-      expect(player).to receive(:receive_damage)
-      object.run(player)
-    end
-  end
-end
-
 feature 'Attacking' do
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play 
     click_link 'Attack'
-    click_link 'OK'
+    click_button 'OK'
+    click_link 'Attack'
+    click_button'OK'
     expect(page).not_to have_content 'Rebecca: 60HP'
     expect(page).to have_content 'Rebecca: 50HP'
   end
 end
 
+feature 'Attacking' do
+  scenario 'reduce Player 1 HP by 10' do
+    sign_in_and_play 
+    click_link 'Attack'
+    click_button 'OK'
+    click_link 'Attack'
+    click_button 'OK'
+    expect(page).not_to have_content 'Clare: 60HP'
+    expect(page).to have_content 'Clare: 50HP'
+  end
+end
 
+feature 'Attacking' do
+  scenario 'attack p2' do
+  sign_in_and_play
+  click_link 'Attack'
+  expect(page).to have_content 'Clare attacked Rebecca'
+  end
+end
+
+feature 'Attacking' do
+  scenario 'attack p1' do
+  sign_in_and_play
+  click_link 'Attack'
+  click_button'OK'
+  click_link 'Attack'
+  expect(page).to have_content 'Rebecca attacked Clare'
+  end
+end
--- Battle/spec/features/switch_turns_spec.rb
+++ Battle/spec/features/switch_turns_spec.rb
@@ -8,7 +8,7 @@ feature 'Switch turns' do
     scenario 'after player 1 attacks' do
       sign_in_and_play
       click_link 'Attack'
-      click_link 'OK'
+      click_button 'OK'
       expect(page).not_to have_content "Clare's turn"
       expect(page).to have_content "Rebecca's turn"
     end
